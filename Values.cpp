835548dd72e3f9e7ba9a9138ac8314e37d626104
Jarne <91670760+glopjevdb@users.noreply.github.com>
2022-11-29 15:54:23 +0100

    add Values.cpp file

---
+++ Values.cpp
@@ -0,0 +1,15 @@
+
+
+
+static double PLAYER_MAX_SPEED = 0.5;
+static double PLAYER_ACCELERATION = 0.0005;
+static double PLAYER_DECELERATION = 0.05;
+static double GRAVITY = 0.001;
+static double JUMP_ACCEL = 1;
+
+static int WINDOW_WIDTH = 600;
+static int WINDOW_HEIGHT = 600;
+
+static int SPRITEWIDTH = 50;
+static int SPRITEHEIGHT = 50;
+
