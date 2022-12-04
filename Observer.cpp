02372739bf5074ef3f5cd0dbbbd0810efc1791e5
Jarne <91670760+glopjevdb@users.noreply.github.com>
2022-12-04 13:23:12 +0100

    add observer documentation

--- Observer.cpp
+++ Observer.cpp
@@ -3,11 +3,20 @@
 //
 
 #include "Observer.h"
-
+/**
+ * @brief Registers an observer to the observable subject
+ * 
+ * @param observer : The object (that must derive from IObserver) to register
+ */
 void IObservable::register_observer(std::shared_ptr<IObserver> observer) {
     observers.push_back(observer);
 }
 
+/**
+ * @brief Removes an observer from the observable subject
+ * 
+ * @param observer : The object (that must derive from IObserver) to remove
+ */
 void IObservable::remove_observer(std::shared_ptr<IObserver> observer) {
     for (auto it = observers.begin(); it != observers.end(); ++it) {
         if (*it == observer) {
@@ -17,6 +26,9 @@ void IObservable::remove_observer(std::shared_ptr<IObserver> observer) {
     }
 }
 
+/**
+ * @brief Notifies all observers of the observable subject
+ */
 void IObservable::notify_observers() {
     for (auto &observer : observers) {
         observer->update();
