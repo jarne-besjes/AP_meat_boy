3d4a8f62dd19ee35ea55abfea6f7b3203236dd6b
Jarne <91670760+glopjevdb@users.noreply.github.com>
2022-12-04 13:23:25 +0100

    add stopwatch documentation

--- Stopwatch.cpp
+++ Stopwatch.cpp
@@ -5,15 +5,26 @@
 #include "Stopwatch.h"
 #include <iostream>
 
+/**
+ * @brief Get the instance of the stopwatch (this will allways return the same instance)
+ * 
+ * @return Stopwatch& : the instance that's returned
+ */
 Stopwatch &Stopwatch::getInstance() {
     static Stopwatch instance;
     return instance;
 }
 
+/**
+ * @brief Starts the stopwatch
+ */
 void Stopwatch::start() {
     current = std::chrono::system_clock::now();
 }
 
+/**
+ * @brief Stops the stopwatch and saves the time difference
+ */
 void Stopwatch::stop() {
     previous = current;
     current = std::chrono::system_clock::now();
